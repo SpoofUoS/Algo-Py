3374c6391ee684d4fcc0f3fcd0bd03268675a208
Spoof-UoS <74731011+Spoof-UoS@users.noreply.github.com>
2021-03-23 22:27:03 +0900

    2021.03.23.ii

--- 최대 수익 구하기.py
+++ 최대 수익 구하기.py
@@ -1,16 +1,17 @@
 def max_profit(prices):
     n = len(prices)
     
-    max_profit = 0
-    min_price = prices[0]
+    max_profit = 0                         # 최대 수익은 항상 0이상
+    min_price = prices[0]                  # 첫째 날의 주가를 최솟값으로 기억
     
-    for i in range(1,n):
-        profit = prices[i] - min_price
+    
+    for i in range(1,n):                   # 1부터 n-1까지 반복
+        profit = prices[i] - min_price     # 지금 까지의 최솟값에 주식을 사서 i날에 팔때의 수익
         
-        if profit > max_profit:
+        if profit > max_profit:            # 이 수익이 지금까지 최대 수익보다 크면 값을 고침
             max_profit = profit
             
-        if prices[i] < min_price:
+        if prices[i] < min_price:          # i날 주가가 최솟값보다 작으면 값으 고정
             min_price = prices[i]
     
     return max_profit
